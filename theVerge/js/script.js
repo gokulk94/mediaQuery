cfe37c4b38bbb90bda9972f3678c42ac82b7fba6
gokulk94 <gokulk@qburst.com>
2015-09-01 19:05:32 +0530

    Started working on theverge

---
+++ theVerge/js/script.js
@@ -0,0 +1,32 @@
+var v = document.getElementById('menu');
+var p = document.getElementById('slide-menu');
+var k = document.getElementById('slide-article');
+var f = document.getElementById('ar');
+var runner = false;
+function run() {
+    runner = !(runner);
+    if(runner == true) {
+        p.className = "menu"+" show-left";
+        console.log(p);
+    } else {
+        p.className = "menu"+" hide-left";
+    }
+}
+
+v.addEventListener('click', run, false);
+var s = document.getElementById('slide-menu');
+s.addEventListener('click', run, false);
+
+var art = false;
+function art_run() {
+    art = !(art);
+    if(art == true) {
+        f.className = "ar"+" show-right";
+        // f.style.position ="absolute"
+    } else {
+        f.className = "ar"+" hide-right";
+        // f.style.position = "fixed";
+    }
+}
+k.addEventListener('click', art_run,false);
+f.addEventListener('click', art_run,false);
